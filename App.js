69581d2766099c7b202037b8210544a1c530c8ac
Иван Решетников <resivalex@gmail.com>
2017-09-24 03:30:46 +0300

    React Native base

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Reports App</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    color: '#006',
+    fontSize: 54
+  }
+});
